d08218c34cc2e17011d3af631724f5b85c691f17
srx <srx92@protonmail.com>
2025-02-13 17:56:59 +0200

    Solution

--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,13 @@
-# Create your urls here
+from rest_framework import routers
+from django.urls import include, path
 
-urlpatterns = []
+from author.views import AuthorViewSet
+
+router = routers.DefaultRouter()
+router.register("authors", AuthorViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "author"
